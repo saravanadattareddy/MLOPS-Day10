a587912ca795fc3270e8ab7b4c4dfb01161e848b
saravanadatta <saravandattareddy251298@gmail.com>
2025-07-30 18:32:20 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="saravana",
+    author_email="saravanadattareddy251298@gmail.com",
+    packages=find_packages()
+)
